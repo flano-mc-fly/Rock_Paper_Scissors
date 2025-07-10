a73c4fa78e69c554a37dd9015bf53d76abe8e2b5
flano-mc-fly <196986928+flano-mc-fly@users.noreply.github.com>
2025-07-10 21:52:13 +0200

    start writing "playRound"-function

--- game.js
+++ game.js
@@ -18,18 +18,33 @@ function getComputerChoice() {
 
 //create a function named "getHumanChoice"
 function getHumanChoice() {
-    //create the variable "humanChoice" and store the answer.
+    //create the variable "answer", store the answer and make it case-insensitive.
     //prompt the question: "Let's play a game. What do you choose? rock, paper or scissors?"
-    let humanChoice = prompt("Let's play a game. What do you choose? rock, paper or scissors?");
-    console.log(humanChoice)
+    let answer = prompt("Let's play a game. What do you choose? rock, paper or scissors?").toLowerCase();
+    
 }
 
 //create variable "humanScore" and set it to 0
-var humanScore = 0
+var humanScore = 0;
 //create variable "computerScore" and set it to 0
-var computerScore = 0
+var computerScore = 0;
 
+//create a function named "playRound"
+//define two parameters for "playRound": "humanChoice" and "computerChoice"
+function playRound (humanChoice, computerChoice) {
+//use these parameters to take human and computer choices as arguments
+getComputerChoice(computerChoice)
+getHumanChoice(humanChoice)
+console.log(humanChoice)
+console.log(computerChoice)
+//rock beats scissors
 
+//paper beats rock
 
+//scissors beat paper
 
+}
+
+//announce the winner
+//increment "humanScore" or "computerScore" variable based on the round winner
 
