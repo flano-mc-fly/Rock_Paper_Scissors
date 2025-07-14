a72423513e9d2b5424a973e783a7bf9ae0a39519
flano-mc-fly <196986928+flano-mc-fly@users.noreply.github.com>
2025-07-14 20:39:54 +0200

    create new function "playGame" put function "playRound" inside it announce game winner in the console

--- game.js
+++ game.js
@@ -20,51 +20,62 @@ function getComputerChoice() {
 function getHumanChoice() {
     //create the variable "answer" and store the answer.
     //prompt the question: "Let's play a game. What do you choose? rock, paper or scissors?"
-    const answer = prompt("Let's play a game. What do you choose? rock, paper or scissors?").toLowerCase();
-    return answer;
+    return prompt("Let's play a game. What do you choose? rock, paper or scissors?").toLowerCase();
 }
+//create function "playGame"
+function playGame() {
+    //create variable "humanScore" and set it to 0
+    let humanScore = 0;
+    //create variable "computerScore" and set it to 0
+    let computerScore = 0;
 
-//create variable "humanScore" and set it to 0
-let humanScore = 0;
-//create variable "computerScore" and set it to 0
-let computerScore = 0;
+    //create a function named "playRound"
+    //define two parameters for "playRound": "humanChoice" and "computerChoice"
+    function playRound (humanChoice, computerChoice) {
+    //use these parameters to take human and computer choices as arguments
 
-//create a function named "playRound"
-//define two parameters for "playRound": "humanChoice" and "computerChoice"
-function playRound (humanChoice, computerChoice) {
-//use these parameters to take human and computer choices as arguments
-
-if (humanChoice === computerChoice) {
-console.log("it's a tie");
+    if (humanChoice === computerChoice) {
+        console.log("it's a tie");
+    }
+    //rock beats scissors
+    else if ((humanChoice === "rock" && computerChoice === "scissors") || 
+    //paper beats rock    
+            (humanChoice === "paper" && computerChoice === "rock") ||
+    //scissors beat paper
+            (humanChoice === "scissors" && computerChoice === "paper"))
+    {
+    //announce the human as winner
+    //increment "humanScore" variable
+        ++humanScore 
+        console.log(`you win! ${humanChoice} beats ${computerChoice}`);
+    }
+    //computer rock beats scissors
+    else if ((computerChoice === "rock" && humanChoice === "scissors") || 
+    //computer paper beats rock    
+            (computerChoice === "paper" && humanChoice === "rock") ||
+    //computer scissors beat paper
+            (computerChoice === "scissors" && humanChoice === "paper"))
+    {
+    //announce the computer as winner
+    //increment "computerScore" variable
+        ++computerScore
+        console.log(`you lose! ${computerChoice} beats ${humanChoice}`);
+    }
+    }
+    //play 5 rounds
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    //announce the game winner
+    if (humanScore > computerScore) {
+        console.log(`You won the game! You got ${humanScore} points. The computer only ${computerScore}`)
+    }
+    else if (computerScore > humanScore){
+        console.log(`Sorry you lost the game! You got only ${humanScore} points. The computer got ${computerScore}`)
+    }
+    else {
+        console.log(`It's a tie! No Winner for this game. Let's play another round`)
+    }
 }
-//rock beats scissors
-else if ((humanChoice === "rock" && computerChoice === "scissors") || 
-//paper beats rock    
-        (humanChoice === "paper" && computerChoice === "rock") ||
-//scissors beat paper
-        (humanChoice === "scissors" && computerChoice === "paper"))
-{
-//announce the human as winner
-//increment "humanScore" variable
-    ++humanScore 
-    console.log(`you win! ${humanChoice} beats ${computerChoice}`);
-}
-//computer rock beats scissors
-else if ((computerChoice === "rock" && humanChoice === "scissors") || 
-//computer paper beats rock    
-        (computerChoice === "paper" && humanChoice === "rock") ||
-//computer scissors beat paper
-        (computerChoice === "scissors" && humanChoice === "paper"))
-{
-//announce the computer as winner
-//increment "computerScore" variable
-    ++computerScore
-    console.log(`you lose! ${computerChoice} beats ${humanChoice}`);
-}
-}
-
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
